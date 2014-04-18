9d76906a006f6f006de98210f16cf874efa68884
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-04-18 10:20:49 -0400

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParticipantTracking::Application.config.secret_key_base = '147687ec524fe3c6491e37e8492b642b01bf42d0cb0a20cc9dfbb46993308601317ff5ea086b7ed510d7477fd26e90de5ddc85aed229ff6af779cad81ee1dd4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ParticipantTracking::Application.config.session_store :cookie_store, key: '_participant_tracking_session'
