1b83ffb753e2675da1705c2bcf61c557bbffdeec
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-04-28 11:34:58 -0400

    constrain api actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ ParticipantTracking::Application.routes.draw do
  
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :participants
-      resources :properties
-      resources :participant_properties
-      resources :participant_types
+      resources :participants, only: [:index, :create]
+      resources :properties, only: [:index]
+      resources :participant_properties, only: [:index, :create]
+      resources :participant_types, only: [:index]
     end
   end 
    
