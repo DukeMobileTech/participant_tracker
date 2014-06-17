58759c44e7ee0169ea1738b328e1dcc4cad3c9c5
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-06-17 14:32:44 -0400

    participant foreign key association

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,5 +1,5 @@
 class Participant < ActiveRecord::Base
-  has_many :participant_properties, foreign_key: :participant_uuid
+  has_many :participant_properties, foreign_key: :participant_uuid, primary_key: :uuid
   belongs_to :participant_type
   delegate :label, to: :participant_type
   
