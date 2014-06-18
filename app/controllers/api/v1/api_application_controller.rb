033b5058bdeabe26a2aacd9d101f3a4dbcdc15e4
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-06-18 14:31:52 -0400

    add api_key model

--- app/controllers/api/v1/api_application_controller.rb
+++ app/controllers/api/v1/api_application_controller.rb
@@ -2,7 +2,25 @@ module Api
   module V1
     class ApiApplicationController < ApplicationController
       protect_from_forgery with: :null_session
+      before_filter :restrict_access
+      before_filter :check_version_code
+      skip_before_filter :authenticate_user_from_token!
       skip_before_filter :authenticate_user!
+      
+      private
+        def restrict_access
+          unless current_user
+            api_key = ApiKey.find_by_access_token(params[:access_token])
+            head :unauthorized unless api_key
+          end
+        end
+
+        def check_version_code
+          if params[:version_code]
+            head :upgrade_required unless params[:version_code].to_i >= Settings.minimum_android_version_code
+          end
+        end
+      
     end
   end
 end
