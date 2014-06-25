cc3e112fd4d17ddc5f4d2d8255ce4c252537d3ec
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-06-25 11:15:18 -0400

    use auth token to find user for api session logout action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class SessionsController < Devise::SessionsController
       skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+      skip_before_filter :restrict_access, only: [:destroy]
+      skip_before_filter :check_version_code, only: [:destroy]
       respond_to :json
       
       def create
@@ -10,9 +12,13 @@ module Api
       end
       
       def destroy
-        warden.authenticate!(:scope => resource_name, :recall => '#{controller_path}#failure')
-        current_user.update_column(:authentication_token, nil)
-        render :json => { :success => true, :info => 'Logged out', :data => {} }, status: :ok
+        current_user = User.find_by_authentication_token(params[:auth_token])
+        if current_user
+          current_user.update_column(:authentication_token, nil)
+          render :json => { :success => true, :info => 'Logged out', :data => {} }, status: :ok
+        else
+          failure
+        end
       end
       
       def failure
