f053b5781d5a665d210f1fdcd5ab91879947bd1c
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-06-27 12:37:18 -0400

    annotate relationships

--- app/models/relationship_type.rb
+++ app/models/relationship_type.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: relationship_types
+#
+#  id                          :integer          not null, primary key
+#  participant_type_owner_id   :integer
+#  participant_type_related_id :integer
+#  label                       :string(255)
+#  created_at                  :datetime
+#  updated_at                  :datetime
+#  deleted_at                  :datetime
+#
+
 class RelationshipType < ActiveRecord::Base
   has_many :relationships
   belongs_to :participant_type_owner, class_name: "ParticipantType", foreign_key: :participant_type_owner_id
