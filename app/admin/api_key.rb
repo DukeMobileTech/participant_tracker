31db205bfa1de79d6a8405e3a174c2ebf22c919b
Michael Nipper <mjn4406@gmail.com>
2014-08-05 15:00:37 -0400

    Allow editing of access token

--- app/admin/api_key.rb
+++ app/admin/api_key.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ApiKey do
+  permit_params :access_token
 
   index do
     column :id
@@ -18,6 +19,11 @@ ActiveAdmin.register ApiKey do
   end
 
   form do |f|
+    unless f.object.new_record?
+      f.inputs "Api Key Details" do
+        f.input :access_token
+      end
+    end
     f.actions
   end
 end
