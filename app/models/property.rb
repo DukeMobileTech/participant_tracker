7651e266152fc9be5b5ee3bd60106a76400aeb80
Michael Nipper <mjn4406@gmail.com>
2015-02-20 11:54:10 -0500

    Remove depricated default scope

--- app/models/property.rb
+++ app/models/property.rb
@@ -16,7 +16,7 @@
 #
 
 class Property < ActiveRecord::Base
-  default_scope order('id ASC')
+  default_scope { order('id ASC') }
 
   has_many :participant_properties, dependent: :destroy
   belongs_to :participant_type
