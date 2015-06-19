e46a01802c1f61d24b47d07af3f8127c58239fdc
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-06-19 13:05:39 -0400

    fix participant_property foreign id bug

--- app/admin/participant_property.rb
+++ app/admin/participant_property.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register ParticipantProperty do
   form do |f|
     f.inputs "Participant Property Details" do
       f.input :uuid
-      f.input :participant, collection: Participant.all.collect {|p| [p.uuid, p.id]} 
+      f.input :participant, collection: Participant.all.collect {|p| [p.uuid, p.uuid]} 
       f.input :property, collection: Property.all.collect {|p| [p.label, p.id]}
       f.input :value
     end
