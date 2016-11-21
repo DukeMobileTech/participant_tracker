ad5934f021787b0512cbd20fd1d8b58df18c547e
Leonard Ng'eno <dranoel.bravo@gmail.com>
2016-11-21 15:54:38 -0500

    capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,15 @@
-lock '3.2.1'
+lock '3.6.1'
 
 set :application, 'participant_tracker'
 set :scm, :git
-set :repo_url, 'git@github.com:DukeMobileTech/participant_tracker.git'
-set :branch, 'master'
-set :deploy_to, '/var/www/participant_tracker'
 set :pty, false
-set :user, 'dmtg'
 set :use_sudo, false
 set :rails_env, 'production'
 set :deploy_via, :copy
-set :ssh_options, { :forward_agent => true, :port => 2222 }
 set :format, :pretty
 set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/secret_token.txt config/local_env.yml}
-set :linked_dirs, fetch(:linked_dirs).push("bin" "log" "tmp/pids" "tmp/cache" "tmp/sockets" "vendor/bundle" "public/system")
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 set :linked_dirs, fetch(:linked_dirs) + %w{ updates }
 
 namespace :deploy do
