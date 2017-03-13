1b378107b20b665719e58a096cf6abca80c80053
Leonard Ng'eno <dranoel.bravo@gmail.com>
2017-03-13 08:03:39 -0400

    scope participant and participant_properties relationship

--- app/admin/participant.rb
+++ app/admin/participant.rb
@@ -1,13 +1,31 @@
 ActiveAdmin.register Participant do
   menu priority: 4
   permit_params :uuid, :participant_type_id
+  sidebar 'Associations', only: :show do
+    ul do
+      li link_to 'Participant Properties', admin_participant_participant_properties_path(resource)
+    end
+  end
+
+  index do
+    selectable_column
+    column :id
+    column :created_at
+    column :updated_at
+    column :uuid
+    column :device_label
+    column 'Participant Type' do |participant|
+      participant.participant_type.label
+    end
+    column 'Participant Label', &:properties_label
+    actions
+  end
 
   form do |f|
-    f.inputs "Participant Details" do
+    f.inputs 'Participant Details' do
       f.input :uuid
-      f.input :participant_type, collection: ParticipantType.all.collect {|p| [p.label, p.id]} 
+      f.input :participant_type, collection: ParticipantType.all.collect { |p| [p.label, p.id] }
     end
     f.actions
   end
-  
 end
--- app/admin/participant_property.rb
+++ app/admin/participant_property.rb
@@ -1,15 +1,28 @@
 ActiveAdmin.register ParticipantProperty do
-  menu priority: 5
+  belongs_to :participant
   permit_params :uuid, :participant_uuid, :property_id, :value
 
+  index do
+    selectable_column
+    column :id
+    column :created_at
+    column :updated_at
+    column :uuid
+    column :participant_uuid
+    column :value
+    column 'Property' do |participant_property|
+      participant_property.property.label
+    end
+    actions
+  end
+
   form do |f|
-    f.inputs "Participant Property Details" do
+    f.inputs 'Participant Property Details' do
       f.input :uuid
-      f.input :participant, collection: Participant.all.collect {|p| [p.uuid, p.uuid]} 
-      f.input :property, collection: Property.all.collect {|p| [p.label, p.id]}
+      f.input :participant, collection: Participant.all.collect { |p| [p.uuid, p.uuid] }
+      f.input :property, collection: Property.all.collect { |p| [p.label, p.id] }
       f.input :value
     end
     f.actions
   end
-  
 end
